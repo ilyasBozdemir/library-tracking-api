943ac333fc73e276634bebcfdf7f5dac1176e5b9
ilyasBozdemir <bozdemir.ib70@gmail.com>
2024-04-15 09:13:15 +0300

    Remove ignored files

--- LibraryTrackingApp/src/frontend/constants/site.jsx
+++ LibraryTrackingApp/src/frontend/constants/site.jsx
@@ -17,98 +17,29 @@ const baseUrl =
     : "http://localhost:3000";
 
 export const site = {
-  title: `Appizsoft | Yazılım & Tasarım & Dijital Pazarlama`,
+  title: ``,
   API_BASE_URL: API_BASE_URL,
   baseUrl: baseUrl,
-  publisher: `Appizsoft Yazılım`,
-  ogImage: "https://appizsoft-static-api.vercel.app/images/og-image.png",
-  twImage: "https://appizsoft-static-api.vercel.app/images/tw-image.png",
+  publisher: ``,
+  ogImage: "",
+  twImage: "",
   logoUrl: baseUrl + "/logo.png",
   imageAlt: "site-logo-png",
-  description: `Dijital ve yazılım çözümleri ortağınız olarak, işletmenize özel stratejiler ve yazılım hizmetleri sunarak dijital ve yazılım alanlarında rekabet avantajı elde etmenizi sağlıyoruz.`,
+  description: ``,
   name: ``,
   keywords: "",
   isRobotIndex: true,
   themeColor: theme.colors.primary,
   lang: "tr",
-  mailAddress: "info@appizsoft.com",
+  mailAddress: "info@domain.com",
   address: {
-    streetAddress: `Karapürçek, 290. Sk. No:56 D:3`,
-    addressLocality: `Altındağ`,
-    addressRegion: `Ankara`,
-    postalCode: `06312`,
+    streetAddress: ``,
+    addressLocality: ``,
+    addressRegion: ``,
+    postalCode: ``,
     addressCountry: `TR`,
   },
-  telephone: "+90 505 367 63 35",
-  sosyalMediaLinks: [
-    {
-      id: 1,
-      label: "instagram",
-      link: "https://www.instagram.com/appizsoftcom/",
-    },
-    {
-      id: 2,
-      label: "facebook",
-      link: "https://www.facebook.com/appizsoftcom/",
-    },
-    {
-      id: 2,
-      label: "threads",
-      link: "https://www.threads.net/appizsoftcom",
-    },
-
-    {
-      id: 3,
-      label: "tiktok",
-      link: "https://www.tiktok.com/@appizsoftcom",
-    },
-    {
-      id: 4,
-      label: "linkedin",
-      link: "https://tr.linkedin.com/company/appizsoft",
-    },
-
-    {
-      id: 5,
-      label: "youtube",
-      link: "https://www.youtube.com/@Appizsoft",
-    },
-    {
-      id: 6,
-      label: "twitter",
-      link: "https://twitter.com/appizsoftcom",
-    },
-    {
-      id: 7,
-      label: "github",
-      link: "https://github.com/appizsoftcom",
-    },
-    {
-      id: 8,
-      label: "pinterest",
-      link: "https://tr.pinterest.com/appizsoftcom/",
-    },
-    {
-      id: 9,
-      label: "dribbble",
-      link: "https://dribbble.com/appizsoftcom",
-    },
-    {
-      id: 10,
-      label: "behance",
-      link: "https://www.behance.net/appizsoftcom",
-    },
-    {
-      id: 11,
-      label: "vimeo",
-      link: "https://vimeo.com/appizsoftcom",
-    },
-    {
-      id: 11,
-      label: "devto",
-      link: "https://dev.to/appizsoftcom",
-    },
-  ],
+ 
   analyticsCodes: {
     pinterestAnalyticsCode,
     googleAnalyticsCode,
