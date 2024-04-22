05eafd58c863b432be852ae078cb9d2215a7faa7
ilyasBozdemir <bozdemir.ib70@gmail.com>
2024-04-22 10:19:57 +0300

    a controlller has been  created for the user's book files,audio or pdf type.

---
+++ LibraryTrackingApp/src/backend/Presentation/LibraryTrackingApp.WebApi/Controllers/v1/EmailController.cs
@@ -0,0 +1,26 @@
+﻿using LibraryTrackingApp.Infrastructure.Mvc;
+
+namespace LibraryTrackingApp.WebApi.Controllers.v1;
+
+[ApiController]
+[ApiVersion(ApiVersions.V1)]
+[Route($"api/v{ApiVersions.V1}/email")]
+public class EmailController : CustomBaseController
+{
+    public EmailController(IMediator mediator) : base(mediator)
+    {
+    }
+
+
+
+    //[HttpPost("send")]
+    //public async Task<IActionResult> SendMail(SendEmailCommandRequest request)
+    //{
+    //    SendEmailCommandResponse commandResponse = await _mediator.Send(request);
+
+    //    return new JsonResult(new { data = commandResponse.Data })
+    //    {
+    //        StatusCode = commandResponse.StatusCode
+    //    };
+    //}
+}
