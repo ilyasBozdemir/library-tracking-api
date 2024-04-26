9cb11dbd16b2c94b1ba6897229ee41421ef070ec
ilyasBozdemir <bozdemir.ib70@gmail.com>
2024-04-26 19:08:34 +0300

    tour component added.

--- LibraryTrackingApp/src/frontend/layouts/Anon/layout.jsx
+++ LibraryTrackingApp/src/frontend/layouts/Anon/layout.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { tourSteps } from "./guides/tourSteps";
+import TourComponent from "@/components/TourComponent";
 
 function Layout({ children }) {
-  return <>
-    <Navbar />
-    {children}
-  </>;
+  return (
+    <>
+      <TourComponent tourSteps={tourSteps} layoutName="mainLayout" />
+      <Navbar />
+      {children}
+    </>
+  );
 }
 
 export default Layout;
