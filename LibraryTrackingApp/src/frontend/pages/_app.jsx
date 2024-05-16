383d629aaeaebad3417ef6acfa8c9a8c89ac5c13
ilyasBozdemir <bozdemir.ib70@gmail.com>
2024-05-16 13:08:04 +0300

    updated theme

--- LibraryTrackingApp/src/frontend/pages/_app.jsx
+++ LibraryTrackingApp/src/frontend/pages/_app.jsx
@@ -8,7 +8,6 @@ const AdminLayout = dynamic(() => import("@/layouts/Admin/layout"));
 const ForumLayout = dynamic(() => import("@/layouts/Forum/layout"));
 const PlaceholderLayout = dynamic(() => import("@/layouts/Placeholder/layout"));
 
-import { useColorMode, colorMode } from "@chakra-ui/react";
 
 import "../styles/globals.css";
 import AOS from "aos";
@@ -20,13 +19,37 @@ import { AuthContextProvider, AuthProvider } from "@/contexts/AuthContext";
 import { AppContextProvider } from "@/contexts/AppContext";
 import { errorStatusCodes } from "@/constants/errorStatusCodes";
 import { site } from "@/constants/site";
+import theme from "@/src/theme";
+
+const AlertData = () => {
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(site.API_BASE_URL + '');
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (<>
+
+
+    <Alert status={error ? 'warning' : 'info'} justifyContent={'center'}>
+      <AlertIcon />
+      <Text as={'p'}>
+        {error ? 'Üzgünüm, veritabanından veri çekilemiyor. Fakat Sayfalar arasında gezinmeye devam edebilirsiniz.' : 'Harika! Veriler başarıyla alındı.'}
+      </Text>
+    </Alert></>)
+}
 
 function MyApp({ Component, pageProps, session, statusCode }) {
   const router = useRouter();
 
-  const theme = extendTheme(
-    colorMode === "light" ? lightTheme.colors : darkTheme.colors
-  );
+
 
   const placeholderRoutes = [
     "/privacy-policy",
@@ -77,19 +100,7 @@ function MyApp({ Component, pageProps, session, statusCode }) {
     AOS.refresh();
   }, []);
 
-  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(site.API_BASE_URL + '');
-      } catch (error) {
-        setError(error.message);
-      }
-    };
-
-    fetchData();
-  }, []);
 
 
   return (
@@ -102,12 +113,12 @@ function MyApp({ Component, pageProps, session, statusCode }) {
         <AppContextProvider>
           <AuthContextProvider>
             <ChakraProvider theme={theme} resetCSS>
-              <Alert status={error ? 'warning' : 'info'} alignContent={'center'}>
-                <AlertIcon />
-                <Text textAlign={'center'}>
-                {error ? 'Üzgünüm, veritabanından veri çekilemiyor. Verileri görmek için backend sunucusunu başlatmanız gerekiyor.' : 'Harika! Veriler başarıyla alındı.'}
-                </Text>
-              </Alert>
+
+{/*
+
+*/}
+
+<AlertData/>
               <Layout>
 
                 <Component {...pageProps} />
