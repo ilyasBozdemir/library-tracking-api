586c07992ce0f9f663407b503e941cf8604cdd63
ilyasBozdemir <bozdemir.ib70@gmail.com>
2024-05-16 13:29:01 +0300

    updated confşg file

--- LibraryTrackingApp/src/frontend/next.config.js
+++ LibraryTrackingApp/src/frontend/next.config.js
@@ -4,19 +4,8 @@ const { i18n } = require("./next-i18next.config");
 const routes = require("./routes");
 
 const nextConfig = {
-  i18n,
   reactStrictMode: true,
 
-  async rewrites() {
-    const rewritesConfig = Object.values(routes).flatMap((route) =>
-      Object.entries(route).map(([path, c]) => ({
-        source: c.config.rewrite.source,
-        destination: c.config.rewrite.destination,
-      }))
-    );
-
-    return rewritesConfig;
-  },
 };
 
 module.exports = nextConfig;
