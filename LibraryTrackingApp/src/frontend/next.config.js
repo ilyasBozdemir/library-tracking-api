51f06dcdce570d6316c4f35321449703ecce46b9
ilyasBozdemir <bozdemir.ib70@gmail.com>
2024-05-16 13:42:04 +0300

    nextjs updated

--- LibraryTrackingApp/src/frontend/next.config.js
+++ LibraryTrackingApp/src/frontend/next.config.js
@@ -5,6 +5,47 @@ const routes = require("./routes");
 
 const nextConfig = {
   reactStrictMode: true,
+  exportTrailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      // paths...
+    };
+  },
+  // Zaman aşımını artırın
+  exportTrailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      // paths...
+    };
+  },
+  // Zaman aşımını artırın
+  exportTrailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      // paths...
+    };
+  },
+  // Zaman aşımını artırın
+  exportTrailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      // paths...
+    };
+  },
+  // Zaman aşımını artırın
+  exportTimeout: 1000, // 1 saniye yerine başka bir değer de kullanabilirsiniz
 
 };
 
